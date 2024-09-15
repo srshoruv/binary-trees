2b1da57d3f4441e9d04be9032edc58fff473546b
srshoruv <srshoruv2002@gmail.com>
2024-09-15 20:10:25 +0600

    tree traversal

---
+++ traversalTree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTrees(vector<int> nodes) {
+    idx++;
+    if (nodes[idx] == -1) {
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+    currNode->left = buildTrees(nodes);
+    currNode->right = buildTrees(nodes);
+
+    return currNode;
+}
+
+void preorder(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node* root = buildTrees(nodes);
+    preorder(root);
+}
